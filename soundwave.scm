f0d6af0d83f68b362a126ff0d6cb0b512e75c434
Kalle Blomström <kalle.blomstrom@gmail.com>
2014-10-16 23:04:33 +0200

    Small update

--- soundwave.scm
+++ soundwave.scm
@@ -1,5 +1,29 @@
-(define (soundwave)
-	(scale 10 1 1)
-	(draw-cube))
+;Define soundwave object
+(define soundwave 
+  (scale 10 1 1)
+  (draw-cube))
 
-(every-frame (soundwave))
+;Function that moves curve vertically
+(define (vertical-move sw))
+
+;Function that moves curve horizontally
+(define (horizontal-move sw))
+
+;Function that changes colour of curve
+(define (change-colour sw))
+
+;Function that advances curve in time
+(define (advance sw))
+
+;Draw loop
+(every-frame 
+  (with-object soundwave
+               ;Vertical movement
+               (vertical-move soundwave)
+               ;Horizontal movement
+               (horizontal-move soundwave)
+               ;Colour
+               (change-colour soundwave)
+               ;Forward speed
+               (advance soundwave))
+  )
